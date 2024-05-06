1a6131afd7342fb35fa18248041bfdef3266d74a
zaineb <bennoumazaineb@gmail.com>
2024-05-06 14:02:48 +0100

    commit 2

--- src/sucess.js
+++ src/sucess.js
@@ -1,38 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
-import axios from 'axios'; // Importez axios ici
+import { useLocation } from 'react-router-dom'; // Import useLocation instead of useSearchParams
+import axios from 'axios';
 import DashboardLayout from 'examples/LayoutContainers/DashboardLayout';
 import DashboardNavbar from 'examples/Navbars/DashboardNavbar';
 import Footer from 'examples/Footer';
 import { base_url } from './utils/baseUrl';
 
 function Success() {
-  const [searchParams] = useSearchParams();
   const [result, setResult] = useState('');
-  const dispatch = useDispatch(); // Vous pouvez utiliser useDispatch si nécessaire
+  const [loading, setLoading] = useState(true);
+  const location = useLocation(); // Use useLocation to access query parameters
 
   useEffect(() => {
-    console.log("Payment ID:", searchParams.get("payment_id")); // Ajout d'un console.log pour afficher l'ID de paiement
-    axios
-      .post(`${base_url}payment/${searchParams.get("payment_id")}`)
-      .then((res) => {
-        console.log("Response data:", res.data); // Ajout d'un console.log pour afficher la réponse de l'API
-        setResult(res.data.result.status);
-      })
-      .catch((err) => console.error(err));
-  }, []); // Déclenchez l'effet lorsque searchParams change
+    const fetchPaymentStatus = async () => {
+      const searchParams = new URLSearchParams(location.search);
+      const paymentId = searchParams.get("payment_id");
+
+      if (paymentId) {
+        try {
+          const token = localStorage.getItem('user');
+          const currentUser = JSON.parse(token);
+          const userName = currentUser ? currentUser.Nom_Prénom : '';
+
+          const response = await axios.post(`${base_url}payment/${paymentId}`, { userName });
+          console.log("Response data:", response.data);
+          setResult(response.data.result?.status );
+        } catch (error) {
+          console.error("Failed to fetch payment status:", error);
+        } finally {
+          setLoading(false);
+        }
+      } else {
+        setLoading(false);
+      }
+    };
+
+    fetchPaymentStatus();
+  }, [location]);
 
   return (
-  
     <DashboardLayout>
     <DashboardNavbar />
     <div className='p-4'>
-      <React.Fragment>
-        {result === 'SUCCESS' && (
-          <div className="alert alert-success">Success</div>
-        )}
-      </React.Fragment>
+      {/* Rendu conditionnel en fonction du résultat du paiement */}
+      {!loading && result === 'SUCCESS' ? (
+        <div className="alert alert-success">Payment Successful</div>
+      ) : (
+        <div >En attente de verification</div>
+      )}
     </div>
     <Footer />
   </DashboardLayout>
