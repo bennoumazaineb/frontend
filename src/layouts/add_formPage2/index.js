3b21746318cacdcc898c96a05d32912da69da7a9
zaineb <bennoumazaineb@gmail.com>
2024-05-08 12:31:03 +0100

    commit56f

--- src/layouts/add_formPage2/index.js
+++ src/layouts/add_formPage2/index.js
@@ -22,6 +22,7 @@ import 'react-quill/dist/quill.snow.css';
 import {getClients} from "feature/auth/authSlice"
 import imageCompression from 'browser-image-compression';
 import { uploadImg } from "feature/upload/uploadSlice";
+import { delImg } from "feature/upload/uploadSlice";
 
 const Add_formPage2 = () => {
   const [notification, setNotification] = useState(false);
@@ -36,7 +37,7 @@ const Add_formPage2 = () => {
   useEffect(() => {
     dispatch(getClients());
   }, [dispatch]);
-  const imgState = useSelector((state) => state.upload?.images2);
+  const imgState = useSelector((state) => state.upload?.images);
   const submitHandler = async (e) => {
     e.preventDefault();
 
@@ -187,7 +188,7 @@ console.log("add_page2",add_page2)
        <MDBox display="flex" flexDirection="row" mt={5} mb={3} width="100%">
             <MDBox width="100%">
 
-                <Dropzone   onDrop={(acceptedFiles) => dispatch(uploadImg2(acceptedFiles))}>
+                <Dropzone   onDrop={(acceptedFiles) => dispatch(uploadImg(acceptedFiles))}>
                   {({ getRootProps, getInputProps }) => (
                     <MDBox
                       {...getRootProps()}
@@ -219,7 +220,7 @@ console.log("add_page2",add_page2)
         {console.log(image.public_id,"sss")}
         <DeleteIcon
           color="black"
-          onClick={() => dispatch(deleteImg2(image.public_id))}
+          onClick={() => dispatch(delImg(image.public_id))}
           style={{ position: 'absolute', top: 0, right: 0, cursor: 'pointer' }}
         />
           <img src={image.url} alt="" width={200} height={200} />
